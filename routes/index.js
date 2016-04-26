57ad6cbd870223ae889aad6518d793f1cb8bd5cc
Willyb15 <william.steen.bryant@gmail.com>
2016-04-26 15:23:25 -0400

    Heroku deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var mongoUrl = process.env.MONGOLAB_URI ||
     // process.env.MONGOHQ_URL ||
-    'mongodb://localhost:27017/test';
+    'mongodb://localhost/test';
 
 var db;
 // var allPhotos;
